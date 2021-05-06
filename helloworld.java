c039917de1bce253e9d0239a978f0abbbd744bdc
sugarnyang <midjhincarry@gmail.com>
2021-05-06 22:39:30 +0900

    VS Code 환경 구축

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * @author sugarnyang
+ * @date 2021/05/06
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
