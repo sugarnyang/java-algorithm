3da4a269f4db18279bbfee172635950c08be1f1a
sugarnyang <midjhincarry@gmail.com>
2021-05-12 23:01:29 +0900

    Create Main.java

---
+++ String(문자열)/3. 문장 속 단어/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+    public String solution(String str) {
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+        String[] s = str.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                answer = x;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(T.solution(str));
+        sc.close();
+        
+    }
+}
