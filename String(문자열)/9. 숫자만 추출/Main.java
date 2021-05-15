9f96b2cb1800f3e7b85f688004212369d0855d02
sugarnyang <midjhincarry@gmail.com>
2021-05-15 23:41:59 +0900

    9. 숫자만 추출

---
+++ String(문자열)/9. 숫자만 추출/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public int solution(String s) {
+        // int answer=0;
+        String answer = "";
+        for (char x : s.toCharArray()) {
+            // if(x>=48 && x<=57) answer=answer*10+(x-48);
+            /*
+             * if(Character.isDigit(x)){ answer=answer*10+ Character.getNumericValue(x); }
+             */
+            if (Character.isDigit(x))
+                answer += x;
+        }
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.print(T.solution(str));
+        sc.close();
+    }
+}
