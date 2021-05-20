d255efe587ef47927fa87a667bdf73ec64a345d3
sugarnyang <midjhincarry@gmail.com>
2021-05-20 23:15:06 +0900

    2. 보이는 학생

---
+++ Array(1, 2차원 배열)/2. 보이는 학생/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public int solution(int n, int[] arr) {
+        int answer = 1, max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(T.solution(n, arr));
+        sc.close();
+    }
+}
