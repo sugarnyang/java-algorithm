b5522773999a704c55ed76f510338c3259825fbf
sugarnyang <midjhincarry@gmail.com>
2021-06-05 23:21:39 +0900

    5. 연속된 자연수의 합(수학)

---
+++ Tow pointers, Sliding window/5. 연속된 자연수의 합(수학)/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public int solution(int n) {
+        int answer = 0, cnt = 1;
+        n--;
+        while (n > 0) {
+            cnt++;
+            n = n - cnt;
+            if (n % cnt == 0)
+                answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.print(T.solution(n));
+    }
+}
