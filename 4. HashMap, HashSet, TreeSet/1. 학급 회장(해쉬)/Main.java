468b81ba61c574c5f05f909b0ba9ef21a780721e
sugarnyang <midjhincarry@gmail.com>
2021-06-09 23:11:39 +0900

    1. 학급 회장(해쉬)

---
+++ 4. HashMap, HashSet, TreeSet/1. 학급 회장(해쉬)/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Main {
+    public char solution(int n, String s) {
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        // System.out.println(map.containsKey('F'));
+        // System.out.println(map.size());
+        // System.out.println(map.remove('C'));
+
+        int max = Integer.MIN_VALUE;
+        for (char key : map.keySet()) {
+            // System.out.println(key+" "+map.get(key));
+            if (map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        sc.close();
+        System.out.println(T.solution(n, str));
+    }
+}
