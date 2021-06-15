58600b2b802a8d0f785dd689a00d8a3894753e5e
sugarnyang <midjhincarry@gmail.com>
2021-06-15 23:08:55 +0900

    1. 올바른 괄호

---
+++ 5. Stack, Queue/1. 올바른 괄호/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(')
+                stack.push(x);
+            else {
+                if (stack.isEmpty())
+                    return "NO";
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty())
+            return "NO";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        System.out.println(T.solution(str));
+    }
+}
