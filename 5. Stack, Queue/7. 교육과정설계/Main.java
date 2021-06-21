21a2a08f3f84f2da48050dde9acca5d2d3715c28
sugarnyang <midjhincarry@gmail.com>
2021-06-21 23:41:10 +0900

    7. 교육과정설계

---
+++ 5. Stack, Queue/7. 교육과정설계/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public String solution(String need, String plan) {
+        String answer = "YES";
+        Queue<Character> Q = new LinkedList<>();
+        for (char x : need.toCharArray())
+            Q.offer(x);
+        for (char x : plan.toCharArray()) {
+            if (Q.contains(x)) {
+                if (x != Q.poll())
+                    return "NO";
+            }
+        }
+        if (!Q.isEmpty())
+            return "NO";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        sc.close();
+        System.out.println(T.solution(a, b));
+    }
+}
