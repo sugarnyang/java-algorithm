9d45213a0270b3dec2e5a6edbc2c69f9ff9dc681
sugarnyang <midjhincarry@gmail.com>
2021-06-27 23:37:35 +0900

    5. 중복확인

---
+++ 6. Sorting and Searching/5. 중복확인/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public String solution(int n, int[] arr) {
+        String answer = "U";
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                answer = "D";
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        System.out.println(T.solution(n, arr));
+        sc.close();
+    }
+}
