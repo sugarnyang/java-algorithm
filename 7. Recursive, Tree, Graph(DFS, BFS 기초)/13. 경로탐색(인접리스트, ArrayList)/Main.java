2ec2a3245c43e867e642626d37ed1424e9e13ec1
sugarnyang <midjhincarry@gmail.com>
2021-07-15 23:44:48 +0900

    13. 경로탐색(인접리스트, ArrayList)

---
+++ 7. Recursive, Tree, Graph(DFS, BFS 기초)/13. 경로탐색(인접리스트, ArrayList)/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    static int n, m, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
+
+    public void DFS(int v) {
+        if (v == n)
+            answer++;
+        else {
+            for (int nv : graph.get(v)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        sc.close();
+        System.out.println(answer);
+    }
+}
